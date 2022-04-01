68b4f76d28b1d4eb8f065fb5df1cd15a742528aa
Jasmine Mills <jazzrmills@gmail.com>
2022-04-01 15:30:42 +0100

    Task 10 completed

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -9,26 +9,14 @@ from io import BufferedIOBase
 def get_pet_shop_name(shop):
     return shop["name"]
 
-# def test_total_cash(self):
-#     sum = get_total_cash(self.cc_pet_shop)
-#     self.assertEqual(1000, sum)
-
 def get_total_cash(cash):
     return cash["admin"]["total_cash"]
 
-# def test_add_or_remove_cash__add(self):
-#         add_or_remove_cash(self.cc_pet_shop,10)
-#         cash = get_total_cash(self.cc_pet_shop)
-#         self.assertEqual(1010, cash)
 
 def add_or_remove_cash(new_amount,cash):
     new_amount["admin"]["total_cash"] += cash
    
     return new_amount
-
-#  def test_pets_sold(self):
-#         sold = get_pets_sold(self.cc_pet_shop)
-#         self.assertEqual(0, sold)
 
 def get_pets_sold(sold):
     return sold ["admin"]["pets_sold"]
@@ -42,8 +30,28 @@ def increase_pets_sold(new_amount,pets):
 def get_stock_count(pet):
     return len(pet["pets"])
 
-def pets_by_breed(breed,amount):
+def get_pets_by_breed(pet_shop,breed):
+    list_of_pets = []
+    pets = pet_shop["pets"]
 
-    list_of_breeds = []
+    for pet in pets:
+        if pet ["breed"] == breed:
+            list_of_pets.append(pet)
+# add item that is being looped
+    return list_of_pets
 
-        for breed in breeds:
+def find_pet_by_name(pet_shop,name):
+    pets = pet_shop["pets"]
+
+    for pet in pets:
+        if pet["name"] == name:
+            return pet
+#     list_of_pets_names = []
+#     pets = pet_shop["pets"]
+
+#     for pet in pets:
+#         if pet ["name"] == name:
+#             list_of_pets_names.append(pet)
+# # add item that is being looped
+#     return list_of_pets_names
+
